e4d10418a00bacb4e4ea4daec2de0adc041f2d7f
Vsevolod Fadeev <sevafad@gmail.com>
2023-05-08 02:53:43 +0300

    Add basic graphic editor.

--- frontend/qt-crocodile/mainwindow.cpp
+++ frontend/qt-crocodile/mainwindow.cpp
@@ -1,13 +1,19 @@
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "paintscene.h"
+#include <QDebug>
+#include <QTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    scene = new PaintScene();
+    ui->graphicsView->setScene(scene);
+    scene->setSceneRect(0,0, ui->graphicsView->width() - 20, ui->graphicsView->height() - 20);
 }
 
 MainWindow::~MainWindow()
