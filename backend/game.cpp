b73a99ab50c7b07b91d6777dcc899bd5c7f6afbd
Alexandr Usankin <hacheriki@gmail.com>
2023-05-20 00:28:49 +0300

    Added game logic

--- backend/game.cpp
+++ backend/game.cpp
@@ -4,5 +4,85 @@
 Game::Game(Server *server)
 {
     this->server = server;
+    playerCount = 0;
+    currentImage = nullptr;
+    isGameRunning = false;
+
+//    connect(server, &Server::connected, this, &Game::waitGame);
+    connect(server, &Server::newMessage, this, &Game::parseMessage);
+    connect(server, &Server::disconnected, this, &Game::removePlayer);
 }
 
+void Game::waitGame(QTcpSocket* socket) {
+    if (isGameRunning) {
+        server->sendMessage(socket, Guesser);
+        return;
+    }
+
+    if (socketToPlayer.count() > 1) {
+        startGame();
+        return;
+    }
+}
+
+void Game::startGame() {
+    currentWord = "test";
+
+    isGameRunning = true;
+    currentDrawer = socketToPlayer.keys()[QRandomGenerator::global()->bounded(0,playerCount)];
+    server->sendMessage(currentDrawer, Drawer, currentWord.toUtf8());
+
+    foreach (auto socket, socketToPlayer.keys()) {
+        server->sendMessage(socket, Guesser);
+    }
+}
+
+void Game::removePlayer(QTcpSocket* socket) {
+    socketToPlayer.remove(socket);
+}
+
+void Game::parseMessage(QTcpSocket* socket, int code, QByteArray message) {
+    switch (code) {
+    case SetNickname: {
+        socketToPlayer[socket] = QString(message);
+        waitGame(socket);
+        break;
+    }
+    case Guess: {
+        checkAnswer(socket, QString(message));
+        break;
+    }
+    case UpdateImage: {
+        updateImage(message);
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+void Game::checkAnswer(QTcpSocket* socket, QString guess) {
+    if (guess != currentWord) {
+        server->sendMessage(socket, WrongAnswer);
+    }
+
+//    QString response =
+//    server->sendMessage(socket, GameEnded);
+    // check input and call endGame
+}
+
+void Game::updateImage(QByteArray &message) {
+    currentImage = &message;
+
+    foreach (auto socket, socketToPlayer.keys()) {
+        if (socket == currentDrawer) continue;
+        server->sendMessage(socket, UpdateImage, *currentImage);
+    }
+    // update drawer image
+}
+
+
+void Game::endGame() {
+
+    // output winner and restart game
+}
